027760fdd45bed079514d8edf8d424361e50eb79
JTTran <tranjt80@gmail.com>
2021-05-02 03:20:08 +0300

    Spring bean scope and life cycle

--- src/com/tranjt/springdemo/TrackCoach.java
+++ src/com/tranjt/springdemo/TrackCoach.java
@@ -22,14 +22,5 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {	
 		return "Just do it: " + fortuneService.getFortune();
 	}
-	
-	// add an init method
-	public void doMyStartupStuff() {
-		System.out.println("TrackCoach: inside method doMyStartupMethod");
-	}
-	
-	// add a destroy method
-	public void doMyDestroyStuff() {
-		System.out.println("TrackCoach: inside method doMyDestroyStuff");
-	}
+
 }
